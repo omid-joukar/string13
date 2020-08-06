8197e63e290faa44f0f1909be30af008d472031e
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-08-06 12:10:20 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Created by KPS on 8/6/2020.
+ */
+public class Main {
+    public static void main(String[] args) {
+        String name1 = "omid";
+        String name2 = "ali";
+        if (name1.equalsIgnoreCase(name2)){
+            System.out.println(name1+" : is equals "+name2);
+        }
+        else{
+            System.out.println(name1+" : is not equals "+name2);
+        }
+    }
+}
